f37f241fd318f03a390c78cad7ff8dbe0b0cae8c
Santanu Sen <nestas.consultancy@gmail.com>
2015-03-17 00:36:57 +0530

    changed assembly information

--- CouchDBLibrary/Properties/AssemblyInfo.cs
+++ CouchDBLibrary/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("CouchDBLibrary")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A C# library to interact with CouchDB")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Santanu Sen, nestas Consultancy")]
 [assembly: AssemblyProduct("CouchDBLibrary")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
